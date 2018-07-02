46fcbcc230b4f50c2b16fc3f441542e90abd5e34
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-07-02 23:47:14 +0400

    small Vector addings

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class Vector {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	plus(vector) {
+		if(vector instanceof this) {
+			throw new Error("Не вектор");
+		}
+
+		return this;
+	}
+}
