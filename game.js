80f5a60b5636560c2f5e494bb73985f4aabdc5f2
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-07-03 09:35:13 +0300

    Vector plus edited

--- game.js
+++ game.js
@@ -7,10 +7,14 @@ class Vector {
 	}
 
 	plus(vector) {
-		if(vector instanceof this) {
+		if(!(vector instanceof Vector)) {
 			throw new Error("Не вектор");
 		}
 
-		return this;
+		this.x += vector.x;
+		this.y += vector.y;
+
+		return new Vector(this.x, this.y);
+		// return this; toDO проверить как лучше
 	}
 }
