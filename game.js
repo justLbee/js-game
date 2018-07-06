b8b16493956e9bfbf0db03d88f24b1e72f1524ea
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2018-07-06 12:40:16 +0300

    isIntersect ready

--- game.js
+++ game.js
@@ -75,15 +75,15 @@ class Actor {
 		let topRight = {
 			x: actor.right,
 			y: actor.top
-		}
+		};
 		let bottomLeft = {
 			x: actor.left,
 			y: actor.bottom
-		}
+		};
 		let bottomRight = {
 			x: actor.right,
 			y: actor.bottom
-		}
+		};
 
 		let topPlayerLeft = {
 			x: this.left,
@@ -92,19 +92,19 @@ class Actor {
 		let topPlayerRight = {
 			x: this.right,
 			y: this.top
-		}
+		};
 		let bottomPlayerLeft = {
 			x: this.left,
 			y: this.bottom
-		}
+		};
 		let bottomPlayerRight = {
 			x: this.right,
 			y: this.bottom
-		}
-		console.log(this.left + ' ' + actor.left);
-		console.log(this.right + ' ' + actor.right);
-		console.log(this.top + ' ' + actor.top);
-		console.log(this.bottom + ' ' + actor.bottom);
+		};
+		// console.log(this.left + ' ' + actor.left);
+		// console.log(this.right + ' ' + actor.right);
+		// console.log(this.top + ' ' + actor.top);
+		// console.log(this.bottom + ' ' + actor.bottom);
 
 		// if (this.left !== actor.left && this.right !== actor.right && this.top !== actor.top &&
 		// 	this.bottom !== actor.bottom) {
@@ -135,15 +135,18 @@ class Actor {
 		// console.log(`Sravnenie: ${this.bottom} >= ${actor.bottom} : ${this.bottom >= actor.bottom}`);
 		// console.log(`Sravnenie: ${this.top} <= ${actor.top} : ${actor.right && this.top <= actor.top}`);
 		// console.log(`${this !== actor}`);
-		console.log(`${((topPlayerLeft.x >= topRight.x && topPlayerLeft.y <= topRight.y) &&
-			(bottomPlayerLeft.x >= topRight.x && bottomPlayerLeft.y >= bottomRight.y)) ||
-		((topPlayerRight.x <= topLeft.x && topPlayerRight.y <= topLeft.y) &&
-			(bottomPlayerRight.x <= bottomLeft.x && bottomPlayerRight.y <= bottomLeft.y))}`);
 
-	// ||
-	// 	(this.left < actor.right && this.right > actor.right)  ||
-	// 	(this.right > actor.left && this.left < actor.left)  ||
-	// 	(this.top < actor.bottom && this.bottom > actor.bottom)
+		console.log(`Sravnenie: ${this.left} >= ${actor.right} : ${this.left >= actor.right}`);
+		console.log(`Sravnenie: ${this.right} <= ${actor.left} : ${this.right <= actor.left}`);
+		console.log(`Sravnenie: ${this.bottom} <= ${actor.top} : ${this.bottom <= actor.top}`);
+		console.log(`Sravnenie: ${this.top} >= ${actor.bottom} : ${this.top >= actor.bottom}`);
+		console.log(`${(this.left >= actor.right || this.right <= actor.left)
+		|| (this.bottom <= actor.top || this.top >= actor.bottom)}`);
+
+		// ||
+		// 	(this.left < actor.right && this.right > actor.right)  ||
+		// 	(this.right > actor.left && this.left < actor.left)  ||
+		// 	(this.top < actor.bottom && this.bottom > actor.bottom)
 
 
 		if (this !== actor) {
@@ -152,23 +155,30 @@ class Actor {
 			// 	&& bottomPlayerRight === bottomRight) && (actor.pos.x < 0 || actor.pos.y < 0)) {
 			// 	return false;
 			// }
-			if (((topPlayerLeft.x >= topRight.x && topPlayerLeft.y <= topRight.y) &&
-				(bottomPlayerLeft.x >= topRight.x && bottomPlayerLeft.y >= bottomRight.y)) ||
-				((topPlayerRight.x <= topLeft.x && topPlayerRight.y <= topLeft.y) &&
-					(bottomPlayerRight.x <= bottomLeft.x && bottomPlayerRight.y <= bottomLeft.y))) {
+			// if((this.left < actor.left && this.right > actor.right && this.top < actor.top
+			// 	&& this.bottom > actor.bottom))
+			// {
+			// 	return true;
+			// }
+			// if((this.left < actor.right && this.right > actor.right)  ||
+			// 	(this.right > actor.left && this.left < actor.left)) {
+			// 	return true;
+			// }
 
+			if ((this.left >= actor.right || this.right <= actor.left)
+				|| (this.bottom <= actor.top || this.top >= actor.bottom)) {
 				return false;
 			}
+			else {
+				if(
+					((this.left === actor.left || this.right === actor.right) && this.top <= actor.top && this.bottom >= actor.bottom) ||
+					((this.bottom === actor.bottom || this.top === actor.top) && this.left <= actor.left && this.right >= actor.right) ){
+					return false;
+				}
+			}
+			return true;
 			// else return false;
 
-			else if((this.left < actor.left && this.right > actor.right && this.top < actor.top
-				&& this.bottom > actor.bottom) ||
-					(this.left < actor.right && this.right > actor.right)  ||
-					(this.right > actor.left && this.left < actor.left)  ||
-					(this.top < actor.bottom && this.bottom > actor.bottom))
-			{
-				return true;
-			}
 
 			// else return false;
 		}
